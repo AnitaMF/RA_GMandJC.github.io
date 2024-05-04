6268d84f9dae77bf29be7b3cd56d318562515a16
Ana Mejia F <anamejiafleisacher@gmail.com>
2024-05-04 14:37:26 +0300

    black

--- Assignments/day_02/guess_number.py
+++ Assignments/day_02/guess_number.py
@@ -1,4 +1,4 @@
-## this is a game for users to guess a number between 1-20 
+## this is a game for users to guess a number between 1-20
 
 
 import random
@@ -6,13 +6,15 @@ import random
 number = random.randrange(1, 21)
 print("I am thinking of a number between 1 to 20")
 guess_try = 0
-while True: 
+while True:
     guess = int(input("What is your guess? :"))
-    guess_try = guess_try + 1 
-    if guess == number: 
-        print(f"You are right, that is the number. Your number of tries was: {guess_try}")
+    guess_try = guess_try + 1
+    if guess == number:
+        print(
+            f"You are right, that is the number. Your number of tries was: {guess_try}"
+        )
         break
-    elif guess < number: 
+    elif guess < number:
         print("The number is bigger ")
-    else: 
+    else:
         print("The number is smaller")
