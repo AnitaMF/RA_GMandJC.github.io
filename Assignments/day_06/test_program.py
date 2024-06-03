0c65b25a71f5c242f0e07729f8ca7937fcc3336d
Ana Mejia F <anamejiafleisacher@gmail.com>
2024-06-03 13:58:47 +0300

    fixing pytest

--- Assignments/day_06/test_program.py
+++ Assignments/day_06/test_program.py
@@ -1,5 +1,6 @@
-import sys
 import csv
+import pytest
+import sys
 
 def check_row_sums(filename):
     with open(filename) as csvfile:
@@ -13,10 +14,14 @@ def check_row_sums(filename):
     
     assert all(0.9 <= row_sum <= 1 for row_sum in row_sums), "Calculation failed: Not all row sums are in the range of 0.9 to 1."
     print("Calculation was successful: All row sums are in the range of 0.9 to 1.")
-    
-# Ensure a filename is provided
-if len(sys.argv) < 2:
-    print("Please provide the CSV filename as an argument.")
-else:
-    filename = sys.argv[1]
+
+def test_check_row_sums():
+    filename = "readCount_bacteria_frequencies.csv"
     check_row_sums(filename)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Please provide the CSV filename as an argument.")
+    else:
+        filename = sys.argv[1]
+        check_row_sums(filename)
